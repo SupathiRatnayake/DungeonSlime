2b9ee22614924eb0a9142ca500e25c3cc76e6e51
Supathi Ratnayake <r.supathi@gmail.com>
2026-07-16 14:07:52 +0530

    Add assets to project using Content.mgcb. Render logo in the window.

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -7,6 +7,7 @@ namespace DungeonSlime
 {
     public class Game1 : Core
     {
+        private Texture2D _logo;
         public Game1() : base("Dungeon Slime", 1280, 720, false)
         {
 
@@ -22,6 +23,7 @@ namespace DungeonSlime
         protected override void LoadContent()
         {
             // TODO: use this.Content to load your game content here
+            _logo = Content.Load<Texture2D>("images/logo");
 
             base.LoadContent();
         }
@@ -41,6 +43,15 @@ namespace DungeonSlime
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            // Begin the sprite batch to prepare for rendering
+            SpriteBatch.Begin();
+
+            // Draw the logo texture
+            SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+
+            // Always end the sprite batch when finished
+            SpriteBatch.End();
+
 
             base.Draw(gameTime);
         }
