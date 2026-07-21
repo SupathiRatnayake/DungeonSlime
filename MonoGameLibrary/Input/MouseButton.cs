bfcd85605ae459db888fb175e0f4e54e6d69bafd
Supathi Ratnayake <r.supathi@gmail.com>
2026-07-21 19:37:43 +0530

    feat(input): handle mouse input
    - Implement MouseButton enum.
    - Implement MouseInfo class.
    - Tracks current and previous mouse states.
    - Track the mouse position.
    - Check the change in mouse position between frames and if it was moved.
    - Detect when mouse buttons are pressed or relesaed.
    - Check if mouse buttons are beign hold down.

---
+++ MonoGameLibrary/Input/MouseButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLibrary.Input;
+
+public enum MouseButton
+{
+    Left,
+    Right,
+    Middle,
+    XButton1,
+    XButton2
+}
